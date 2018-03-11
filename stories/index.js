0dd02311fbd8cf17a6cf28efd649c21da2547e48
David Lee <david.isaac.lee@gmail.com>
2018-03-11 16:41:21 -0400

    Update stories

--- stories/index.js
+++ stories/index.js
@@ -6,37 +6,60 @@ import { withState } from '@dump247/storybook-state';
 import DragCapture, { RelativeDragCapture } from '../src';
 import MeasureBounds from '../src/MeasureBounds';
 
-const Cursor = styled.span`
+const AbsoluteCursor = styled.span.attrs({
+	style: ({ position }) => ({
+		left: `${position.x}px`,
+		top: `${position.y}px`,
+	}),
+})`
 	width: 10px;
 	height: 10px;
 
 	background-color: black;
 
 	position: absolute;
-	${({position}) => css`
-		left: ${position.x * 100}%;
-		top: ${position.y * 100}%;
-		transform: translate(-50%, -50%);
-	`}
+	transform: translate(-50%, -50%);
+`;
+
+const Cursor = styled.span.attrs({
+	style: ({ position }) => ({
+		left: `${position.x * 100}%`,
+		top: `${position.y * 100}%`,
+	}),
+})`
+	width: 10px;
+	height: 10px;
+
+	background-color: black;
+
+	position: absolute;
+	transform: translate(-50%, -50%);
 `;
 
 storiesOf('DragCapture', module)
-  .add('basic', () => (
-		<div>
-			<DragCapture
-				shouldBeginDrag={() => true}
-				dragDidBegin={action("began drag")}
-				dragDidMove={action("moved drag")}
-				dragDidEnd={action("ended drag")}
-			>
-				<div style={{
-					width: 300,
-					height: 300,
-					backgroundColor: '#eee',
-				}} />
-		</DragCapture>
-	</div>
-  ))
+  .add('basic', withState({}, (store) => (
+		<DragCapture
+			shouldBeginDrag={() => true}
+			dragDidBegin={(cursorID, position) => store.set({ [cursorID]: position })}
+			dragDidMove={(cursorID, position) => store.set({ [cursorID]: position })}
+			dragDidEnd={(cursorID, position) => store.set({ [cursorID]: undefined })}
+		>
+			<div style={{
+				width: 300,
+				height: 300,
+				backgroundColor: '#eee',
+			}}>
+			{
+				Object.keys(store.state)
+				.map(cursorID => ({ cursorID, position: store.state[cursorID] }))
+				.filter(({ position }) => position != null)
+				.map(({ cursorID, position }) => (
+					<AbsoluteCursor key={cursorID} position={position} />
+				))
+			}
+		</div>
+	</DragCapture>
+  )))
   .add('relative position', withState({}, (store) => (
 		<RelativeDragCapture
 			dragDidBegin={(cursorID, position) => store.set({ [cursorID]: position })}
