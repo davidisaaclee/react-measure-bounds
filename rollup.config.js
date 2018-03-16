f3b6df796b2457f91e2b1319f653ca33525e83ac
David Lee <david.isaac.lee@gmail.com>
2018-03-16 14:54:36 -0400

    Fix external dependencies in Rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,24 @@
 import path from 'path';
 import babel from 'rollup-plugin-babel';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'src/index.js',
   output: {
     name: 'DragCapture',
+		globals: {
+			'react': 'React',
+		},
   },
   plugins: [
     babel({
       exclude: path.join(__dirname, 'node_modules/**')
-    })
+    }),
+		peerDepsExternal(),
+		commonjs(),
+		resolve(),
   ],
 };
 
